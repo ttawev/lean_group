8151f43f7b6250d9607995e17e3590450ace9974
ttawev <ttawev@list.ru>
2023-02-02 19:32:04 +0600

    first_upload

---
+++ js/script.js
@@ -0,0 +1,64 @@
+let burger = document.querySelector('.BurgerMenu');
+let nav = document.querySelector('.nav__list');
+let lang = document.querySelector('.lang__item');
+burger.addEventListener('click', function () {
+    burger.classList.toggle('active')
+    nav.classList.toggle('active')
+});
+lang.addEventListener('click', function () {
+    lang.classList.toggle('active')
+});
+
+$(document).ready(function(){
+    $("#home-slider").owlCarousel({
+        items: 1,
+        autoplay: true,
+        autoplayTimeout: 3500,
+        smartSpeed: 1500,
+        loop: true,
+    }
+    );
+    let owl = $('#cert-slider');
+    owl.owlCarousel({
+        items: 5,
+        margin: 28,
+    });
+    // Go to the next item
+    $('.owl-next').click(function() {
+        owl.trigger('next.owl.carousel');
+    })
+    // Go to the previous item
+    $('.owl-prev').click(function() {
+        // With optional speed parameter
+        // Parameters has to be in square bracket '[]'
+        owl.trigger('prev.owl.carousel', [300]);
+    });
+
+    $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+        $(this)
+          .addClass('active').siblings().removeClass('active')
+          .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+      });
+
+    let products = $('.products-slider');
+    products.owlCarousel({
+        items: 5,
+        margin: 20,
+        loop: true,
+    }
+    );
+    // Go to the next item
+    $('.products-next').click(function() {
+        products.trigger('next.owl.carousel');
+    })
+    // Go to the previous item
+    $('.products-prev').click(function() {
+        // With optional speed parameter
+        // Parameters has to be in square bracket '[]'
+        products.trigger('prev.owl.carousel', [300]);
+    });
+
+  });
+
+
+
